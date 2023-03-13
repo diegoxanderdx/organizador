d1d9d9988f40f80411954c6986d54a787a32a11f
Diegomez-dev <diegoagc@yahoo.es>
2023-03-12 21:13:08 -0500

    refactorized send email to service object and background job

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -35,6 +35,7 @@ Rails.application.configure do
   # config.active_storage.service = :test
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
 
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
@@ -46,5 +47,5 @@ Rails.application.configure do
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
-  config.factory_bot.definition_file_paths = []
+  # config.factory_bot.definition_file_paths = []
 end
